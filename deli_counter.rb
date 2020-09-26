2523ba985d6a91270605325aa0cf95bff46273a4
Alie Stone <helloaliestone@gmail.com>
2020-09-26 16:30:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,8 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
+
+def line(number)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
